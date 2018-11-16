3aec197429680820dbd525cfb4340efa5a91d331
mateuszmacholl <mateuszmacholl0@gmail.com>
2018-11-17 00:04:03 +0100

    refactoring factory context

--- src/main/kotlin/mateuszmacholl/uploadmaker/utils/FactoryContext.kt
+++ src/main/kotlin/mateuszmacholl/uploadmaker/utils/FactoryContext.kt
@@ -5,15 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired
 // I have no idea in which package it should be
 abstract class FactoryContext<type> {
     protected var list: MutableList<type> = mutableListOf()
+        @Autowired
+        private set(list)  {
+            field = list
+        }
 
-    fun <conversionGoal  : type> get(typeClass: Class<conversionGoal>): conversionGoal{
+    fun <conversionGoal : type> get(typeClass: Class<conversionGoal>): conversionGoal {
         val goal = list.firstOrNull { typeClass.isInstance(it) } ?: throw IllegalArgumentException()
         @Suppress("UNCHECKED_CAST") // safety is implied by the high-level program logic
         return goal as conversionGoal
-    }
-
-    @Autowired
-    fun set(list: MutableList<type>){
-        this.list = list
     }
 }
